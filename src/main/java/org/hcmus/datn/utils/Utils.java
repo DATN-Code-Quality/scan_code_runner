391dc1269a68289c0f2613514db1c9a945a17674
vinhpna1c <anhvinhphan659@gmail.com>
2023-03-30 23:06:44 +0700

    add MANIFEST.MF + handle URL file from GG Drive

--- src/main/java/org/hcmus/datn/utils/Utils.java
+++ src/main/java/org/hcmus/datn/utils/Utils.java
@@ -5,4 +5,21 @@ public class Utils {
       String[] names=  fileUrl.split("/");
       return names[names.length-1];
     }
+    //make sure driveURL is link of specified file
+    public static String getLinkFileFromGGDriveShare(String driveURL)
+    {
+        String url="https://drive.google.com/uc?id=";
+        String fileID="";
+        String[] elements=url.split("/");
+
+        for(int i=0;i<elements.length;i++)
+        {
+            if(elements[i].compareTo("d")==0)
+            {
+                fileID=elements[i+1];
+                break;
+            }
+        }
+        return url+fileID;
+    }
 }
