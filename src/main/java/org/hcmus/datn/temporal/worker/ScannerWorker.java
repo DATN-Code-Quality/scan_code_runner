3857afb739ad95a53a6416cc4bada235553d459d
TranNgocVy <ngocvy04042001@gmail.com>
2023-05-23 12:33:17 +0700

    set WorkerOptions

--- src/main/java/org/hcmus/datn/temporal/worker/ScannerWorker.java
+++ src/main/java/org/hcmus/datn/temporal/worker/ScannerWorker.java
@@ -4,6 +4,7 @@ import io.temporal.client.WorkflowClient;
 import io.temporal.serviceclient.WorkflowServiceStubs;
 import io.temporal.worker.Worker;
 import io.temporal.worker.WorkerFactory;
+import io.temporal.worker.WorkerOptions;
 import org.hcmus.datn.common.Config;
 import org.hcmus.datn.temporal.activities.ScannerActivitiesImpl;
 import org.hcmus.datn.temporal.workflow.ScannerWorkflowImpl;
@@ -19,10 +20,18 @@ public class ScannerWorker {
         WorkflowServiceStubs service = WorkflowServiceStubs.newLocalServiceStubs();
         WorkflowClient client = WorkflowClient.newInstance(service);
         WorkerFactory factory = WorkerFactory.newInstance(client);
-        Worker worker = factory.newWorker(SCANNER_TASKQUEUE);
+
+        WorkerOptions.Builder optionBuilder = WorkerOptions.newBuilder();
+        optionBuilder.setMaxConcurrentActivityTaskPollers(1);
+        optionBuilder.setMaxConcurrentActivityExecutionSize(1);
+//        optionBuilder.setMaxConcurrentWorkflowTaskExecutionSize(1);
+
+        Worker worker = factory.newWorker(SCANNER_TASKQUEUE, optionBuilder.build());
 
         worker.registerWorkflowImplementationTypes(ScannerWorkflowImpl.class);
         worker.registerActivitiesImplementations(new ScannerActivitiesImpl());
+
+
 
         factory.start();
     }
